4333e74e7d6337174efe341519262f9a25e98b74
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2023-06-20 14:28:25 -0300

    feat: improve gifts page

--- src/app/gifts/page.tsx
+++ src/app/gifts/page.tsx
@@ -19,12 +19,12 @@ export default async function GiftsPage() {
           <h1 className="mb-4 font-serif text-lg">Lista de Presentes</h1>
           <p className="mb-8 text-sm">
             Queremos que se sintam à vontade para nos presentear de acordo com
-            suas preferências e possibilidades. Preparamos uma seleção de
-            produtos que precisamos na nossa nova casinha. Além disso, pensando
-            em facilitar a contribuição para aqueles que preferem presentear de
-            maneira mais flexível, também disponibilizamos a opção de cotas de
-            PIX. Explore sem restrições, certamente qualquer presente escolhido
-            com carinho será muito apreciado.
+            suas preferências e possibilidades. Preparamos uma seleção de itens
+            que precisamos na nossa nova casinha. E, para facilitar a
+            contribuição daqueles que preferem presentear de maneira mais
+            flexível, também disponibilizamos a opção de cotas de PIX. Explore
+            sem restrições, certamente qualquer presente escolhido com carinho
+            será muito apreciado.
           </p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
