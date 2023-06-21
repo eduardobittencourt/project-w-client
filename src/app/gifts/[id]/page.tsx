86b8d8acf1d0d58ccd701f1150b5989651c69034
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2023-06-21 16:41:41 -0300

    feat: add product link

--- src/app/gifts/[id]/page.tsx
+++ src/app/gifts/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import selectGift from "@/resources/actions/buyGift";
 import { getGift } from "@/resources/getGift";
@@ -45,6 +46,25 @@ export default async function GiftPage({ params }: GiftPageProps) {
             ))}
           </tbody>
         </table>
+        <div className="mb-8 flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
+            <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
+          </svg>
+
+          <Link
+            href={gift.result.link}
+            target="_blank"
+            className="border-b border-white text-sm hover:border-red"
+          >
+            Link para comprar o produto
+          </Link>
+        </div>
 
         <h2 className="mb-4 font-serif text-sm">Avisar sobre a compra</h2>
         <p className="mb-2 text-xs">
@@ -69,10 +89,6 @@ export default async function GiftPage({ params }: GiftPageProps) {
           >
             Confirmar compra
           </button>
-
-          <span className="inline-block w-full pt-4 text-center text-xs text-red">
-            Prefere fazer um PIX? Clique aqui
-          </span>
         </form>
       </div>
     </div>
