2c84b7c3d2cce18af848ec11e8e4c31e6c91b326
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2023-06-28 21:03:05 -0300

    chore: try to log some type of error in production

--- src/app/quotas/[id]/error.tsx
+++ src/app/quotas/[id]/error.tsx
@@ -5,13 +5,12 @@ export default function QuotaError({ reset, error }: QuotaErrorProps) {
   return (
     <div className="flex flex-col items-center">
       <h2 className="mb-8 font-serif text-lg">Ooops!</h2>
-      <p className="mb-4 text-center text-sm">
+      <p className="mb-8 text-center text-sm">
         Tivemos algum problema na confirmação da compra da cota. Por favor,
         verifique se todos os códigos informados estão exatamente como no
         convite que mandamos e, em caso positivo, por favor nos envie uma
         mensagem para que possamos te ajudar a solucionar o problema.
       </p>
-      <span className="mb-8 text-red">{error.message}</span>
       <button
         className="w-full bg-red px-6 py-4 text-center text-sm text-white md:w-auto"
         onClick={reset}
