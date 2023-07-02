a8c3e6691f1dd5b42f3ab5fcc93947317514e95a
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2023-07-02 18:16:53 -0300

    Feat: Success Screen Photo

--- src/app/confirmation/thanks/page.tsx
+++ src/app/confirmation/thanks/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 
+import Pietro from "@/assets/success.png";
 export default function ConfirmationThanksPage() {
   return (
     <main className="flex flex-col items-center">
       <h2 className="mb-8 font-serif text-hg">YAY!</h2>
+      <Image src={Pietro} alt="Foto do Pietro" />
       <p className="mb-8 max-w-xl text-center text-sm">
         Estamos ansiosos para celebrarmos esse momento especial ao seu lado!
       </p>
